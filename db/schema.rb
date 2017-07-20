80b76d59eefff6e63ad6d1ea18cbf00a1b683acd
trupti19 <trupti.its.me@gmail.com>
2017-07-20 12:26:29 +0000

    new update

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@
 ActiveRecord::Schema.define(version: 20170712175511) do
 
   create_table "admins", force: :cascade do |t|
-    t.string   "email",                  default: "kulkarni.trupti.v.19@gmail.com", null: false
-    t.string   "encrypted_password",     default: "Kulkas12025",                    null: false
+    t.string   "email",                  default: "waleedsajid41991@gmail.com@gmail.com", null: false
+    t.string   "encrypted_password",     default: "mypassword",                    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -95,7 +95,7 @@ ActiveRecord::Schema.define(version: 20170712175511) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
+    t.datetime "created_atf"
     t.datetime "updated_at"
     t.boolean  "admin"
     t.string   "role"
